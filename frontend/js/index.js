a3a27a164f85dfc3452d91c2093c04f0d7d0717f
Liz <lizriderwilson@gmail.com>
2021-07-20 11:21:25 -0400

    Fix styling on cards

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -16,10 +16,11 @@ function sanitizeName(name) {
 
 function createColumn(column) {
   let columnName = document.createElement('h3')
+  columnName.setAttribute('class', 'is-size-4 mb-2')
   columnName.innerText = column.name;
 
   let columnInnerDiv = document.createElement('div')
-  columnInnerDiv.setAttribute('class', 'has-text-centered has-background-light')
+  columnInnerDiv.setAttribute('class', 'has-text-centered has-background-light p-3')
   columnInnerDiv.append(columnName);
 
   let boardColumn = document.createElement('div');
@@ -30,8 +31,9 @@ function createColumn(column) {
 }
 
 function createCard(card) {
-  let cardColumn = document.getElementById('column' + card.column_id);
-  console.log(cardColumn);
+  //console.log(card.column_id)
+  let cardColumn = document.getElementById("column" + card.column_id).firstChild;
+  //console.log(cardColumn);
 
   let cardName = document.createElement('h4');
   cardName.innerText = card.name;
@@ -40,7 +42,7 @@ function createCard(card) {
   cardDescription.innerText = card.description;
 
   let columnCard = document.createElement('div');
-  columnCard.setAttribute('class', 'box');
+  columnCard.setAttribute('class', 'box has-background-white-bis');
   columnCard.setAttribute('id', 'card' + card.id);
   columnCard.append(cardName, cardDescription);
   cardColumn.append(columnCard);
